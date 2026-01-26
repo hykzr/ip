bb47cf29cf61dbcdbde138a7de1f0db005384509
hykzr <phhhhhhhhh135@gmail.com>
2026-01-26 21:37:55 +0800

    Level 0: rename chatbot to Yuan and add greetings

---
+++ src/main/java/Yuan.java
@@ -0,0 +1,13 @@
+public class Yuan {
+    public static void main(String[] args) {
+        String greetings = """
+                ____________________________________________________________
+                 Hello! I'm Yuan.
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(greetings);
+    }
+}
