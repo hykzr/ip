6554f6a2483885d7c836cb058f0791b18bebad91
hykzr <phhhhhhhhh135@gmail.com>
2026-01-30 16:36:36 +0800

    Add task and list task

--- src/main/java/Yuan.java
+++ src/main/java/Yuan.java
@@ -16,14 +16,23 @@ public class Yuan {
         String bye = "Bye. Hope to see you again soon!";
         print(greetings);
 
+        // array to store each user input tasks
+        ArrayList<Task> tasks = new ArrayList<Task>();
+
         // echo until "bye" is received
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                print(String.join("\n",
+                        IntStream.range(0, tasks.size())
+                                .mapToObj(i -> (i + 1) + "." + tasks.get(i))
+                                .toList()));
             } else {
                 print(input);
+                tasks.add(new Task(input));
             }
         }
 
