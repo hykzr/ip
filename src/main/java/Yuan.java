964a1e2b3fc0ea3d34db6bee28b64003f524caea
hykzr <phhhhhhhhh135@gmail.com>
2026-01-30 16:33:23 +0800

    Add echo message and bye exit

--- src/main/java/Yuan.java
+++ src/main/java/Yuan.java
@@ -1,13 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class Yuan {
+    private static void print(String message) {
+        System.out.println("____________________________________________________________");
+        System.out.println(message);
+        System.out.println("____________________________________________________________\n");
+    }
+
     public static void main(String[] args) {
         String greetings = """
-                ____________________________________________________________
-                 Hello! I'm Yuan.
-                 What can I do for you?
-                ____________________________________________________________
-                 Bye. Hope to see you again soon!
-                ____________________________________________________________
-                """;
-        System.out.println(greetings);
+                Hello! I'm Yuan.
+                What can I do for you?""";
+        String bye = "Bye. Hope to see you again soon!";
+        print(greetings);
+
+        // echo until "bye" is received
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                print(input);
+            }
+        }
+
+        print(bye);
+        scanner.close();
     }
 }
